60229664e8bffbe8478774e47acd6eecc8836321
MaheshVerma12 <maheshcverma1223@gmail.com>
2024-01-30 19:18:12 +0545

    First commit

---
+++ android/app/src/main/kotlin/com/example/worksmart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.worksmart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/homePage.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Counter app with Bloc pattern"),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "0",
+            style: TextStyle(fontSize: 20),
+          ),
+          const SizedBox(height: 40),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              FloatingActionButton(
+                onPressed: () {},
+                child: Icon(Icons.add),
+              ),
+              FloatingActionButton(
+                onPressed: () {},
+                child: Icon(Icons.remove),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
